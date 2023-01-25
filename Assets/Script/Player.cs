ae069d7eb58f3c80f60ac481769b7469f3422aa2
unknown <gperonnet@gamingcampus.local>
2023-01-25 16:28:58 +0100

    update

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Animation animation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animation = GetComponent<Animation>();
     }
 
     // Update is called once per frame
