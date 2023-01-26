b5fe8fb8b10e4576373ae909507c87d0f49a260b
Gaëtan Peronnet <gaetan.peronnet@gmail.com>
2023-01-26 21:17:44 +0100

    movement v1 and deleted animator

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,19 +4,56 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody rigidbody;
-    public Vector3 playerVelocity;
-    public float playerSpeed = 2.0f;
+    //for return rigidbody of player 
+    Rigidbody playerRb;
+
+    //speed of player on different states 
+    public float speedWalk;
+    public float speedRun;
+    public float speedRotate;
+
+
+    //to choose directional key in Unity
+    public string inputFront;
+    public string inputBack;
+    public string inputLeft;
+    public string inputRight;
+    public string inputJump;
+
+    //for the jump height
+    public Vector3 jumpHeight;
+
+    //for return the capsule collider of player
+    CapsuleCollider playerCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        playerRb = gameObject.GetComponent<Rigidbody>();
+        playerCollider = gameObject.GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if(Input.GetKey(inputFront))
+        {
+            transform.Translate(0,0,speedWalk * Time.deltaTime);
+        }
+
+        if(Input.GetKey(inputBack))
+        {
+            transform.Translate(0,0,-(speedWalk/2) * Time.deltaTime);
+        }
+
+        if(Input.GetKey(inputLeft))
+        {
+            transform.Rotate(0,-speedRotate * Time.deltaTime,0);
+        }
+
+        if (Input.GetKey(inputRight))
+        {
+            transform.Rotate(0, speedRotate * Time.deltaTime, 0);
+        }
     }
 }
